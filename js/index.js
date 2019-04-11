b0e15406223a4a208f62d937fa609ac1f427c3ec
Ben Q <Quackenbush117@gmail.com>
2019-04-10 21:13:38 -0600

    Half way done.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,35 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navContent = document.getElementsByTagName("a");
+navContent[0].textContent = siteContent.nav["nav-item-2"];
+navContent[1].textContent = siteContent.nav["nav-item-2"];
+navContent[2].textContent = siteContent.nav["nav-item-3"];
+navContent[3].textContent = siteContent.nav["nav-item-4"];
+navContent[4].textContent = siteContent.nav["nav-item-5"];
+navContent[5].textContent = siteContent.nav["nav-item-6"];
+
+let ctaImgID = document.getElementById("cta-img")
+console.log(ctaImgID)
+ctaImgID.src = "img/header-img.png";
+
+let ctaText = document.getElementsByClassName("cta-text")
+
+console.log(ctaText)
+
+ctaText[0].textContent = siteContent.cta["h1"];
+
+let ctaButton = document.querySelectorAll(".cta-text")
+
+//ctaButton[1].textContent = siteContent.cta["button"]
+//console.log(ctaButton[1])
+
+
+let featuresHeader = document.querySelectorAll(".text-content")
+
+console.log(featuresHeader[0].children[1])
+
+featuresHeader[0].children[0].textContent = siteContent["main-content"]["features-h4"]
+featuresHeader[0].children[1].textContent = siteContent["main-content"]["features-content"]
+
